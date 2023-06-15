0a5c35d7e91167508aab197792003a587e457f81
OdynoE <odinakaekeneme@gmail.com>
2023-06-15 13:37:10 -0700

    LL

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,10 @@
 /**
  * main - 4-puts.c
  * description - Write a C program that prints exactly
- *
  * Return: Always 0 (success)
  */
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
-
 	return (0);
 }
