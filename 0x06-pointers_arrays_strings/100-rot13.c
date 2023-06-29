d144205d333a51532439f9cf53ad4e33879a21df
OdynoE <odinakaekeneme@gmail.com>
2023-06-29 14:23:19 -0700

    LL

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rot13 - ratate characters 13 places
+ * @str: pointer to strind
+ * Return: str
+ */
+char *rot13(char *str)
+{
+	char *a = str;
+	char *letters = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+	char *rotated = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
+	int i;
+
+	while (*a != '\0')
+	{
+		i = 0;
+		while (letters[i] != '\0')
+		{
+			if (*a == letters[i])
+			{
+				*a = rotated[i];
+				break;
+			}
+			i++;
+		}
+		a++;
+	}
+	return (str);
+}
---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_number - printing an integer
+ * @n: integer
+ */
+void print_number(int n)
+{
+	unsigned int x;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		x = (unsigned int)-n;
+	}
+	else
+		x = (unsigned int)n;
+	if (x / 10 != 0)
+		print_number(x / 10);
+	_putchar(x % 10 + '0');
+}
